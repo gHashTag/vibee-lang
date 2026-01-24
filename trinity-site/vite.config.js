4651c17e14826835109bb37ad27ab574dbb94887
Dmitriy Vasilev <raoffonom@icloud.com>
2026-01-24 20:16:57 +0700

    Fix CI: Simplify vite.config.js (remove defineConfig dependency)

--- trinity-site/vite.config.js
+++ trinity-site/vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
+export default {
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173
   }
-})
+}
