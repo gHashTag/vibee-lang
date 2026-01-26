bc3ef897a0eb1005a4404fe0b65591d0edebac9e
Dmitriy Vasilev <raoffonom@icloud.com>
2026-01-26 18:19:19 +0700

    fix: update brew formula to use HEAD

--- docs/dist/brew/vibee.rb
+++ docs/dist/brew/vibee.rb
@@ -1,8 +1,7 @@
 class Vibee < Formula
   desc "Specification-first programming language for Trinity OS"
   homepage "https://github.com/gHashTag/vibee-lang"
-  url "https://github.com/gHashTag/vibee-lang/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  head "https://github.com/gHashTag/vibee-lang.git", branch: "main"
   license "MIT"
 
   depends_on "zig" => :build
